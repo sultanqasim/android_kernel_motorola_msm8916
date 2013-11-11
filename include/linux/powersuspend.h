d2a1a865e9015ef87c1bb12d1b016abe00096e77
yank555-lu <yank555.lu@gmail.com>
2013-11-11 01:48:32 -0600

    kernel/power/powersuspend: remove userspace dependency from powersuspend
    make powersuspend not depend on a userspace initiator anymore, but use a hook
    in autosleep instead.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -3,6 +3,11 @@
  * Copyright (C) 2007-2008 Google, Inc.
  * Copyright (C) 2013 Paul Reioux 
  *
+ * Modified by Jean-Pierre Rasquin <yank555.lu@gmail.com>
+ *
+ *   make powersuspend not depend on a userspace initiator anymore,
+ *   but use a hook in autosleep instead.
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -19,6 +24,9 @@
 
 #include <linux/list.h>
 
+#define POWER_SUSPEND_INACTIVE	0
+#define POWER_SUSPEND_ACTIVE	1
+
 struct power_suspend {
 	struct list_head link;
 	void (*suspend)(struct power_suspend *h);
@@ -27,6 +35,8 @@ struct power_suspend {
 
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
+
+void set_power_suspend_state_hook(int new_state);
 
 #endif
 
--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -10,6 +10,10 @@
 #include <linux/mutex.h>
 #include <linux/pm_wakeup.h>
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include "power.h"
 
 static suspend_state_t autosleep_state;
@@ -105,8 +109,14 @@ int pm_autosleep_set_state(suspend_state_t state)
 	if (state > PM_SUSPEND_ON) {
 		pm_wakep_autosleep_enabled(true);
 		queue_up_suspend_work();
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks
+#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
