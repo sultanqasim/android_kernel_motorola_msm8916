c05a19954e17e602be9c615c29718a9a92e3de94
Ping Li <pingli@codeaurora.org>
2015-02-03 12:44:40 -0800

    msm: mdss: Fix uninitialized return value for compact ioctl function
    Initialize the return value before using or returning to user space
    to avoid returning uninitialised values to user space.
    
    Change-Id: Iac43744830d725cf69120603befc3e4dd758d031
    Signed-off-by: Ping Li <pingli@codeaurora.org>

--- drivers/video/msm/mdss/mdss_compat_utils.c
+++ drivers/video/msm/mdss/mdss_compat_utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  * Copyright (C) 1994 Martin Schaller
  *
  * 2001 - Documented with DocBook
