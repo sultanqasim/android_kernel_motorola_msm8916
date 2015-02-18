0727081e74d69811fa62f3169ffe37ab2f4a1713
Sujeet Kumar <ksujeet@codeaurora.org>
2015-02-18 16:36:40 +0530

    USB: phy: Set default USB system clock to 80MHz
    Clock driver increase the USB system clock under
    nominal range from 80MHz to 100Mhz. Phy drivers
    use the max clock rate to be set during probe.
    
    Set the default clock rate at 80MHz so that it
    can be increased only when 100Mhz is needed.
    
    Change-Id: Ib0fa07bb7af5909be0f6c2f4d68e66412fdfe130
    Signed-off-by: Sujeet Kumar <ksujeet@codeaurora.org>
    Signed-off-by: Ravi Vembu <raviv@motorola.com>
    Reviewed-on: http://gerrit.mot.com/749433
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -82,6 +82,8 @@
 #define MAX_CHGR_RETRY_COUNT    3
 #define USB_DEFAULT_SYSTEM_CLOCK 80000000	/* 80 MHz */
 
+#define USB_DEFAULT_SYSTEM_CLOCK 80000000	/* 80 MHz */
+
 enum msm_otg_phy_reg_mode {
 	USB_PHY_REG_OFF,
 	USB_PHY_REG_ON,
