4e6f891c192ce59a6f9a050ae70325cb34702933
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-01-16 23:42:19 -0500

    USB: gadget: make MIDI optional

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -42,7 +42,9 @@
 #ifdef CONFIG_SND_PCM
 #include "f_audio_source.c"
 #endif
+#ifdef CONFIG_SND_RAWMIDI
 #include "f_midi.c"
+#endif
 #include "f_mass_storage.c"
 #define USB_ETH_RNDIS y
 #include "f_diag.c"
@@ -2899,6 +2901,7 @@ static struct android_usb_function usbnet_function = {
 	.ctrlrequest	= usbnet_function_ctrlrequest,
 };
 
+#ifdef CONFIG_SND_RAWMIDI
 static int midi_function_init(struct android_usb_function *f,
 					struct usb_composite_dev *cdev)
 {
@@ -2952,6 +2955,7 @@ static struct android_usb_function midi_function = {
 	.bind_config	= midi_function_bind_config,
 	.attributes	= midi_function_attributes,
 };
+#endif
 
 static struct android_usb_function *supported_functions[] = {
 	&ffs_function,
@@ -2979,7 +2983,9 @@ static struct android_usb_function *supported_functions[] = {
 #ifdef CONFIG_SND_PCM
 	&audio_source_function,
 #endif
+#ifdef CONFIG_SND_RAWMIDI
 	&midi_function,
+#endif
 	&uasp_function,
 	&charger_function,
 	&usbnet_function,
