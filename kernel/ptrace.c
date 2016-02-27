093355123208f39ee2c0016dc35b2b13f5fa750f
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-02-27 17:20:35 -0500

    ptrace: silence warning about inadequate permissions
    It seems to spam logs when testing on the stock ROM.
    Not sure if it is a problem with the 3.10 backport of this
    permission check or if Android is doing sketchy things.

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -230,7 +230,9 @@ static int __ptrace_may_access(struct task_struct *task, unsigned int mode)
 	kgid_t caller_gid;
 
 	if (!(mode & PTRACE_MODE_FSCREDS) == !(mode & PTRACE_MODE_REALCREDS)) {
+		/*
 		WARN(1, "denying ptrace access check without PTRACE_MODE_*CREDS\n");
+		 */
 		return -EPERM;
 	}
 
