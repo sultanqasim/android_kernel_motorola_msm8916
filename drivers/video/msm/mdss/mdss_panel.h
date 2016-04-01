7ee960391b7ecbead1c410ed406cccfb246489e4
Shiyong Li <a22381@motorola.com>
2016-03-31 17:39:13 -0700

    Revert "(CR): msm/mdss: support panel High Brightness Mode"
    This reverts commit 00875ef82df1499be0d6d8560fc4405649c2ff6e.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_panel.h
    
    Change-Id: Ib0c209f0f71699fa851f3e75f3fc56f089809e0f
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: http://gerrit.mot.com/840575
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Zachary Crosby <zac@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -188,7 +188,6 @@ struct mdss_intf_recovery {
  * @MDSS_EVENT_REGISTER_RECOVERY_HANDLER: Event to recover the interface in
  *					case there was any errors detected.
  * @MDSS_EVENT_SET_CABC: Set CABC mode, for Motorola "Dynamic CABC" feature.
- * @MDSS_EVENT_ENABLE_HBM: Enable Motorola High Brightness Mode feature.
  */
 enum mdss_intf_events {
 	MDSS_EVENT_RESET = 1,
@@ -213,7 +212,6 @@ enum mdss_intf_events {
 	MDSS_EVENT_REGISTER_RECOVERY_HANDLER,
 	MDSS_EVENT_ENABLE_TE,
 	MDSS_EVENT_SET_CABC,
-	MDSS_EVENT_ENABLE_HBM,
 };
 
 struct lcd_panel_info {
@@ -442,8 +440,6 @@ struct mdss_panel_info {
 
 	bool dynamic_cabc_enabled;
 	enum cabc_mode cabc_mode;
-	bool hbm_feature_enabled;
-	bool hbm_state;
 	bool blank_progress_notify_enabled;
 };
 
