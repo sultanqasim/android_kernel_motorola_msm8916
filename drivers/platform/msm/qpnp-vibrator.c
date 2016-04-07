b84716b63e7f3acb6f3a4c4a9457d669887b2807
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-04-07 15:37:35 -0400

    qpnp-vibrator: remove unused vib_dev variable

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -74,8 +74,6 @@ struct qpnp_vib {
 	struct mutex lock;
 };
 
-static struct qpnp_vib *vib_dev;
-
 static ssize_t qpnp_vib_level_show(struct device *dev,
                                         struct device_attribute *attr,
                                         char *buf)
@@ -448,8 +446,6 @@ static int qpnp_vibrator_probe(struct spmi_device *spmi)
 		return rc;
 
 	device_create_file(vib->timed_dev.dev, &dev_attr_vtg_level);
-
-	vib_dev = vib;
 
 	if (vib->boot_up_vibe)
 		qpnp_vib_enable(&vib->timed_dev, vib->boot_up_vibe);
