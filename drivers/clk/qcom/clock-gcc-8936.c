6b81c0ae8c711f1638cdff0030cf50ed67421d5a
NanMetal <cuppispineta@live.com.ar>
2017-05-04 19:11:52 -0300

    ARM: msm: dts: Overclock GPU to 700 MHz

--- drivers/clk/qcom/clock-gcc-8936.c
+++ drivers/clk/qcom/clock-gcc-8936.c
@@ -827,7 +827,7 @@ static struct rcg_clk vfe0_clk_src = {
 };
 
 static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
-	F(  19200000,	      gcc_xo,   1,	  0,	0),
+	F(  19200000,	   gcc_xo,           1,	  0,	0),
 	F(  50000000,	   gpll0_out_main,  16,	  0,	0),
 	F(  80000000,      gpll0_out_main,  10,	  0,	0),
 	F( 100000000,      gpll0_out_main,   8,	  0,	0),
@@ -835,10 +835,12 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 200000000,      gpll0_out_main,   4,	  0,	0),
 	F( 220000000,      gpll3_out_main,   5,	  0,	0),
 	F( 266670000,      gpll0_out_main,   3,	  0,	0),
-	F( 310000000,	gpll2_gfx3d,	3,	  0,	0),
+	F( 310000000,      gpll2_gfx3d,      3,	  0,	0),
 	F( 400000000,      gpll0_out_main,   2,	  0,	0),
-	F( 465000000,      gpll2_gfx3d,   2,    0,    0),
-	F( 550000000,      gpll3_out_main,   2,    0,    0),
+	F( 465000000,      gpll2_gfx3d,      2,   0,    0),
+	F( 550000000,      gpll3_out_main,   2,   0,    0),
+	F( 650000000,      gpll3_out_main,   2,   0,    0),
+	F( 700000000, 	   gpll3_out_main,   2,   0,    0),
 	F_END
 };
 
@@ -852,7 +854,7 @@ static struct rcg_clk gfx3d_clk_src = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP4(LOW, 220000000, NOMINAL, 400000000, NOMINAL_PLUS, 465000000, HIGH,
-			550000000),
+			700000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
