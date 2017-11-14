46bbce3ed1f9300b4e6328dc216db846b9e23f45
Benjamin Chan <bkchan@codeaurora.org>
2017-11-14 00:27:17 -0500

    msm: mdss: adjust mdss_mdp_get_plane_sizes parameters init order
    Parameter mdss_mdp_plane_sizes must be cleared to 0 before returning
    under an error condition, otherwise caller function will use the
    uninitialized mdss_mdp_plane_sizes values and caused incorrect
    operation.
    
    Bug: 71501679
    Change-Id: I856b17ce9e917cc450040463ec34b7309d34b9b5
    Signed-off-by: Benjamin Chan <bkchan@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_util.c
+++ drivers/video/msm/mdss/mdss_mdp_util.c
@@ -333,6 +333,8 @@ int mdss_mdp_get_plane_sizes(u32 format, u32 w, u32 h,
 	if (ps == NULL)
 		return -EINVAL;
 
+	memset(ps, 0, sizeof(struct mdss_mdp_plane_sizes));
+
 	if ((w > MAX_IMG_WIDTH) || (h > MAX_IMG_HEIGHT))
 		return -ERANGE;
 
@@ -341,7 +343,6 @@ int mdss_mdp_get_plane_sizes(u32 format, u32 w, u32 h,
 		return -EINVAL;
 
 	bpp = fmt->bpp;
-	memset(ps, 0, sizeof(struct mdss_mdp_plane_sizes));
 
 	if (bwc_mode) {
 		u32 height, meta_size;
