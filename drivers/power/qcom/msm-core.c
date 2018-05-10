ad1d9c0e6e3102012cae8b0f6693850b84c82e6a
Kishor PK <kpbhat@codeaurora.org>
2018-05-10 16:04:55 +0530

    msm-core: Add spinlock for update lock
    Ensure spin lock and unlock are added for update lock.
    
    Change-Id: Ica1ccb639c900295ffc29a94d1094a19bfa43755
    Signed-off-by: Kishor PK <kpbhat@codeaurora.org>

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -96,6 +96,7 @@ struct cpu_static_info {
 
 static DEFINE_MUTEX(policy_update_mutex);
 static DEFINE_MUTEX(kthread_update_mutex);
+static DEFINE_SPINLOCK(update_lock);
 static struct delayed_work sampling_work;
 static struct completion sampling_completion;
 static struct task_struct *sampling_task;
@@ -194,7 +195,6 @@ void trigger_cpu_pwr_stats_calc(void)
 {
 	int cpu;
 	static long prev_temp[NR_CPUS];
-	static DEFINE_SPINLOCK(update_lock);
 	struct cpu_activity_info *cpu_node;
 
 	if (disabled)
@@ -363,8 +363,8 @@ static int update_userspace_power(struct sched_params __user *argp)
 	/* Copy the same power values for all the cpus in the cpumask
 	 * argp->cpumask within the cluster (argp->cluster)
 	 */
-	spin_lock(&update_lock);
 	get_user(cpumask, &argp->cpumask);
+	spin_lock(&update_lock);
 	for (i = 0; i < MAX_CORES_PER_CLUSTER; i++, cpumask >>= 1) {
 		if (!(cpumask & 0x01))
 			continue;
@@ -386,8 +386,8 @@ static int update_userspace_power(struct sched_params __user *argp)
 			repopulate_stats(cpu);
 		}
 	}
+	spin_unlock(&update_lock);
 	mutex_unlock(&policy_update_mutex);
-
 	activate_power_table = true;
 	return 0;
 
